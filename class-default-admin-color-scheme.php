fdebfe9342ca0d80bc8f318aca08f762a5c8f522
Barry Ceelen <barry@multipop.org>
2015-09-25 14:19:01 +0200

    Fix load_plugin_textdomain function

--- class-default-admin-color-scheme.php
+++ class-default-admin-color-scheme.php
@@ -230,14 +230,10 @@ class Default_Admin_Color_Scheme {
 	/**
 	 * Load the plugin text domain for translation.
 	 *
-	 * @since    1.0.0
+	 * @since 1.0.0
 	 */
 	public function load_plugin_textdomain() {
-		$domain = 'default-admin-color-scheme';
-		$locale = apply_filters( 'plugin_locale', get_locale(), $domain );
-
-		load_textdomain( $domain, trailingslashit( WP_LANG_DIR ) . $domain . '/' . $domain . '-' . $locale . '.mo' );
-
+		load_plugin_textdomain( 'default-admin-color-scheme', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
 	}
 
 	/**
