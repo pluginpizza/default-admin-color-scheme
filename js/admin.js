6b4a8a189d02ca19d5412037fce802a6db265566
Barry Ceelen <barry@multipop.org>
2022-05-17 00:47:55 +0200

    Refactor admin.js

--- js/admin.js
+++ js/admin.js
@@ -7,9 +7,9 @@
 
  /* global ajaxurl */
 
-(function($){
+( function( $ ){
 	'use strict';
-	$(function() {
+	$( function() {
 
 		var $colorpicker, $stylesheet, currentStylesheetUrl;
 
@@ -21,51 +21,42 @@
 		$stylesheet          = $( '#colors-css' );
 		currentStylesheetUrl = $( '#colors-css' ).attr( 'href' );
 
-		$colorpicker
-			.on( 'hover.colorpicker', '.color-option', function() {
+		$colorpicker.on( 'click.colorpicker', '.color-option', function() {
+			var colors,
+				$this = $(this);
 
-				var colors,
-					$this = $( this );
+			if ( $this.hasClass( 'selected' ) ) {
+				return;
+			}
 
-				// Load the colors stylesheet.
-				$stylesheet.attr( 'href', $this.children( '.css_url' ).val() );
+			$this.siblings( '.selected' ).removeClass( 'selected' );
+			$this.addClass( 'selected' ).find( 'input[type="radio"]' ).prop( 'checked', true );
 
-				// Repaint icons.
-				if ( typeof wp !== 'undefined' && wp.svgPainter ) {
-					try {
-						colors = $.parseJSON( $this.children( '.icon_colors' ).val() );
-					} catch ( error ) {
+			// Load the colors stylesheet.
+			// The default color scheme won't have one, so we'll need to create an element.
+			if ( 0 === $stylesheet.length ) {
+				$stylesheet = $( '<link rel="stylesheet" />' ).appendTo( 'head' );
+			}
+			$stylesheet.attr( 'href', $this.children( '.css_url' ).val() );
 
-					}
+			// Repaint icons.
+			if ( typeof wp !== 'undefined' && wp.svgPainter ) {
+				try {
+					colors = JSON.parse( $this.children( '.icon_colors' ).val() );
+				} catch ( error ) {}
 
-					if ( colors ) {
-						wp.svgPainter.setColors( colors );
-						wp.svgPainter.paint();
-					}
+				if ( colors ) {
+					wp.svgPainter.setColors( colors );
+					wp.svgPainter.paint();
 				}
-			})
-			.on( 'mouseleave.colorpicker', '.color-option', function() {
-				$stylesheet.attr( 'href', currentStylesheetUrl );
-			})
-			.on( 'click.colorpicker', '.color-option', function() {
+			}
 
-				var $this = $( this );
-
-				if ( $this.hasClass( 'selected' ) ) {
-					return;
-				}
-
-				currentStylesheetUrl = $this.children( '.css_url' ).val();
-
-				$this.siblings( '.selected' ).removeClass( 'selected' );
-				$this.addClass( 'selected' ).find( 'input[type="radio"]' ).prop( 'checked', true );
-
-				// Update default color scheme.
-				$.post( ajaxurl, {
-					action:       'save-default-color-scheme',
-					color_scheme: $this.children( 'input[name="admin_color"]' ).val(),
-					nonce:        $( '#color-nonce' ).val()
-				});
+			// Update default color scheme.
+			$.post( ajaxurl, {
+				action:       'save-default-color-scheme',
+				color_scheme: $this.children( 'input[name="admin_color"]' ).val(),
+				nonce:        $( '#color-nonce' ).val()
 			});
+		});
 	});
-})(jQuery);
+})( jQuery );
