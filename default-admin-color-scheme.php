7aa44a71c0e546ca987b85d648b261db035c9de4
Barry Ceelen <barry@multipop.org>
2026-01-02 12:01:10 +0100

    Fix required php and wp version

--- default-admin-color-scheme.php
+++ default-admin-color-scheme.php
@@ -12,8 +12,8 @@
  * Plugin URI:        https://wordpress.org/plugins/default-admin-color-scheme
  * Text Domain:       default-admin-color-scheme
  * Version:           1.0.3
- * Requires PHP:      5.3.0
- * Requires at least: 3.8.0
+ * Requires PHP:      5.3
+ * Requires at least: 3.8
  * GitHub Plugin URI: https://github.com/plugin-pizza/default-admin-color-scheme
  *
  * @package PluginPizza\DefaultAdminColorScheme
